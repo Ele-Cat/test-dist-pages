fec334a9d1c34d5860439c01dab704af6659e1c3
lijiamao <951572198@qq.com>
2024-05-10 11:37:31 +0800

    ✨推送dist修改

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="项目打包文件部署" />
 </template>
 
 <style scoped>
